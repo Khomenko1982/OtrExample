a82be9b6f8a6958204fdcf5b03e8314f94391003
«Andrey» <khomehko@mail.ru>
2024-06-18 13:25:16 +0300

    Initial commit

--- src/main/java/IpRange.java
+++ src/main/java/IpRange.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class IpRange {
+import java.util.Scanner;
+
+public class IpRange {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int size = 4;
+        int array[] = new int[size];
+        System.out.println("Введите начальный ip:");
+            for (int i = 0; i < size; i++) {
+            array[i] = input.nextInt();
+        }
+        System.out.print ("Начальный ip:");
+        for (int i = 0; i < size; i++) {
+            System.out.print ("." + array[i]);
+        }
+        System.out.println();
+//        int[] ip1 = {192, 168, 0, 1};
+//        int[] ip2 = {192, 168, 0, 10};
+    }
 }
