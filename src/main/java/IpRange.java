0a69c22a00c5edaf2d755706f2a48b36c4b67c21
«Andrey» <khomehko@mail.ru>
2024-06-19 14:02:04 +0300

    14:01

--- src/main/java/IpRange.java
+++ src/main/java/IpRange.java
@@ -2,20 +2,31 @@ import java.util.Scanner;
 
 public class IpRange {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int size = 4;
-        int array[] = new int[size];
-        System.out.println("Введите начальный ip:");
-            for (int i = 0; i < size; i++) {
+        Scanner input = new Scanner(System.in).useDelimiter("[\\s.;]+");;
+        int sizeIn = 4;
+        int sizeOut = 4;
+        int array[] = new int[sizeIn];
+        System.out.println("Введите начальный ip диапазона:");
+            for (int i = 0; i < sizeIn; i++) {
             array[i] = input.nextInt();
         }
-        System.out.print ("Начальный ip:");
-        for (int i = 0; i < size; i++) {
+        System.out.print ("Начальный ip диапазона:");
+        for (int i = 0; i < sizeIn; i++) {
             System.out.print ("." + array[i]);
         }
         System.out.println();
-//        int[] ip1 = {192, 168, 0, 1};
-//        int[] ip2 = {192, 168, 0, 10};
+
+        System.out.println("Введите конечный ip диапазона:");
+        for (int i = 0; i < sizeOut; i++) {
+            array[i] = input.nextInt();
+        }
+        System.out.print ("Конечный ip диапазона:");
+        for (int i = 0; i < sizeOut; i++) {
+            System.out.print ("." + array[i]);
+        }
+        System.out.println();
+
+
         Scanner: input.close();
 
     }
