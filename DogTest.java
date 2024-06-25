ea20f8468f4fe1188cb05b531bfc4eff001b1403
«Andrey» <khomehko@mail.ru>
2024-06-25 10:50:44 +0300

    10:50

---
+++ DogTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class DogTest {
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setAge() {
+    }
+}
